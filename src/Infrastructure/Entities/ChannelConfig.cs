e0947ae500a51aa5d0eba8f102a3be6930a4a717
Minh Chanh <chanh.au@alta.com.vn>
2024-11-11 08:35:20 +0700

    change config

--- src/Infrastructure/Entities/ChannelConfig.cs
+++ src/Infrastructure/Entities/ChannelConfig.cs
@@ -13,7 +13,7 @@ public class ChannelConfig
         return new()
         {
             Id = "@DRBTSolana",
-            ReadMessageId = 553860,
+            ReadMessageId = 555035,
         };
     }
 }
